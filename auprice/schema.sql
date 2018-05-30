754b9d51f4ee9c95a0d809adcadf80f356a476fe
pioneer <pioneer@pioneers-Mac-mini.local>
2018-05-30 20:15:39 +0800

    first commit

---
+++ auprice/schema.sql
@@ -0,0 +1,28 @@
+-- drop table if exists pricelists;
+-- create table pricelists (
+--   id integer primary key autoincrement,
+--   datetime timestamp not null,
+--   price_cn float not null
+-- );
+-- drop table if exists history_prices;
+-- create table history_prices (
+--   id integer primary key autoincrement,
+--   datetime timestamp not null,
+--   utctime integer not null,
+--   price_cn float not null,
+-- );
+
+drop table if exists trades;
+create table trades (
+  id integer primary key autoincrement,
+  category integer not null, 
+  weight float not null,
+  create_time timestamp,
+  create_price float not null,
+  create_status boolean not null,
+  end_time timestamp,
+  end_price float,
+  end_status boolean, 
+  profit float,
+  year_ratio float
+);
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='auprice',
+    packages=['auprice'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
